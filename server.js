808a9dd0660e04bd8a37ae6ab4edd99ab49bfcf2
ylesandale <ylesandale7@gmail.com>
2023-03-06 02:59:19 +0400

    fix: фикс разрачи статики

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static("./dist"));
 
-app.listen(PORT, function () {
-  console.log(`Port: ${PORT}!`);
+app.get("*/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Port: ${PORT}!`);
 });
